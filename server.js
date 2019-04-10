a6d10bec2288baf53762adf0376b6597b476b770
Abrahamzick Orozco <aorozcoz79@hotmail.com>
2019-04-10 10:22:19 -0500

    Primer Commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+
+const hbs = require('hbs');
+require('./hbs/helpers')
+
+const port = process.env.PORT || 8080
+
+app.use(express.static(__dirname + '/public'))
+
+// Express HBS Engine
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs')
+
+
+
+app.get('/', function(req, res) {
+
+    res.render('home', {
+        nombre: 'aBrahamzick oroZco'
+    })
+})
+
+app.get('/about', function(req, res) {
+
+    res.render('about', {
+        nombre: 'Abrahamzick',
+        anio: new Date().getFullYear()
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones por el puerto ${port}`);
+})
